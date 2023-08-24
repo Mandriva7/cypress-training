4dcd0f7cebeb0fbe9b281b5c1d224d2bb663f24c
Mandriva7 <cristianbedoya1991@gmail.com>
2023-08-23 22:09:23 -0500

    cambios

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,17 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     video: false,
     screenshotOnRunFailure: false,
     chromeWebSecurity: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-       // e2e testing node events setup code
-   config.defaultCommandTimeout= 10000;
-   config.responseTimeout= 20000;
-   config.pageLoadTimeout= 60000;
-   return config;
+   setupNodeEvents(on, config) {
+     // e2e testing node events setup code
+     config.defaultCommandTimeout= 10000;
+     config.responseTimeout= 20000;
+     config.pageLoadTimeout= 60000;
+     return config;
+     return config;
     },
   },
 });
