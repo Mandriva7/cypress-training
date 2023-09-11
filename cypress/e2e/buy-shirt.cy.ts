7e9589c729f6e71c147b46d6335e343f3f4f2062
Mandriva7 <cristianbedoya1991@gmail.com>
2023-09-11 13:56:11 -0500

    ccc

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -8,14 +8,14 @@
      loginPage.visitLoginPage();
      loginPage.signIn("standard_user", "secret_sauce");
         cy.get("#item_1_title_link > .inventory_item_name").select;//(3)
-        cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();//(4)
+        cy.get("[data-test=\"add-to-cart-sauce-labs-bolt-t-shirt\"]").click();//(4)
         cy.get(".shopping_cart_link").click();//(5)
-        cy.get('[data-test="checkout"]').click();//(6)
-        cy.get('[id="first-name"]').type("Cypress"); //(7)
-        cy.get('[id="last-name"]').type("Workshop"); //(8)
-        cy.get('[id="postal-code"]').type("00000"); //(9)
+        cy.get("[data-test=\"checkout\"]").click();//(6)
+        cy.get("#first-name").type("Cypress"); //(7)
+        cy.get("#last-name").type("Workshop"); //(8)
+        cy.get("#postal-code").type("00000"); //(9)
         cy.get("#continue").click();//(10)
-        cy.get('[id="finish"]').click();//(11)
+        cy.get("[id=\"finish\"]").click();//(11)
         cy.get("#contents_wrapper > .checkout_complete_container > h2").should(
             "have.text",
             "Thank you for your order!"
