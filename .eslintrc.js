9e33df96f572c93e4bacfc6b12556fac2d7f0a5e
Mandriva7 <cristianbedoya1991@gmail.com>
2023-09-20 18:42:31 -0500

    cc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+    root: true,
     "env": {
         "browser": true,
         "es2021": true
@@ -8,8 +9,7 @@ module.exports = {
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
-        "project": ["/Users/cristianbedoya/Desktop/cypress/cypress-training/tsconfig.json"]
-    },
+        "project": ["./tsconfig.json"]},
     "plugins": [
         "@typescript-eslint",
         "cypress"
@@ -22,7 +22,6 @@ module.exports = {
         "cypress/no-force": "warn",
         "no-unused-vars": "warn",
         "require-jsdoc": "warn",
-        "max-len": [ "error", { "code": 120 } ]
+        "max-len": [ "error", { "code": 180 } ]
     },
-
  }
